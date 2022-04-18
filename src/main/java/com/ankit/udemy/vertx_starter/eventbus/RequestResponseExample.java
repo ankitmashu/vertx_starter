6a9236419d58407abc5b7e4a712bd8287dd6e7ba
Ankit Singh <ankitmashu@gmail.com>
2022-04-18 17:05:27 +0530

    Updated on 18/4/22

---
+++ src/main/java/com/ankit/udemy/vertx_starter/eventbus/RequestResponseExample.java
@@ -0,0 +1,46 @@
+package com.ankit.udemy.vertx_starter.eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.EventBus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestResponseExample {
+//private static final Logger LOG= LoggerFactory.getLogger(Re)
+  public static void main(String[] args) {
+    Vertx vertx =Vertx.vertx();
+    vertx.deployVerticle(new RequestVerticle());
+    vertx.deployVerticle(new ResponseVerticle());
+  }
+  static class RequestVerticle extends AbstractVerticle{
+    private static final Logger LOG=LoggerFactory.getLogger(RequestVerticle.class);
+    public static final String ADDRESS = "my.request.address";
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      //super.start(startPromise);
+      startPromise.complete();
+      EventBus eventBus=vertx.eventBus();
+      String message= "Hello World!";
+      LOG.debug("Sending : {}",message);
+      eventBus.<String>request(ADDRESS,message, reply-> {
+        LOG.debug("Response: {}",reply.result().body());
+      });
+    }
+  }
+  static class ResponseVerticle extends AbstractVerticle{
+    private static final Logger LOG=LoggerFactory.getLogger(ResponseVerticle.class);
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().<String>consumer(RequestVerticle.ADDRESS,message -> {
+        LOG.debug("Received Message: {}", message.body());
+        message.reply("Received your message. Thanks!");
+      } );
+
+    }
+  }
+}
